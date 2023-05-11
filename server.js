3d8d092530efb00d96bbb1cc30bd739706839d55
Katya Daron <katya_daron@hotmail.com>
2023-05-11 18:41:17 -0400

    Edited server.js

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
 app.use(express.static(`${__dirname}`))
 
-const questions = ['What is your name?', 'What is your favorite color?', 'What is your favorite food?'];
+// const questions = ['What is your name?', 'What is your favorite color?', 'What is your favorite food?'];
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
