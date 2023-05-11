9435c45363cdd4bde6478fba99649ccf98405fd6
Katya Daron <katya_daron@hotmail.com>
2023-05-11 17:13:41 -0400

    Edited server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(express.static(`${__dirname}`))
 
 const questions = ['What is your name?', 'What is your favorite color?', 'What is your favorite food?'];
 
